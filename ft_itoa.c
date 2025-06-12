ecfc31cbb79897ff5f62cb37b20c68523a8165ff
Ömer Orhan <omerelitok.05@gmail.com>
2025-06-12 13:37:09 +0300

    Implement conversion function: ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: OelitokO <omerelitok.05@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/12 13:04:45 by OelitokO          #+#    #+#             */
+/*   Updated: 2025/06/12 13:34:00 by OelitokO         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_num_len(int n)
+{
+	int	len;
+
+	len = (n <= 0);
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nb;
+	int		len;
+
+	nb = (long)n;
+	len = ft_num_len(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (nb == 0)
+		str[0] = '0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 0)
+	{
+		str[--len] = '0' + (nb % 10);
+		nb /= 10;
+	}
+	return (str);
+}
